0634fa335e0f60d6fbfd6583528c29ca6766c7cc
Mohammed Amin Shaikh <m.shaikh@elsevier.com>
2017-01-18 22:05:11 +0000

    fix: Checking upper and lower bounds for number of random names requested
    Not able to mark move cursor in windows git bash console, hence sticking with feature instead of bug
    fix

--- src/index.js
+++ src/index.js
@@ -8,13 +8,12 @@ module.exports = {
 }
 
 function random(number){
-    if(number === undefined){
+    if(number === undefined || number > starWarNames.length || number < 0){
         return randomItem();    
     }else{
         var randomArray = [];
         for(var i=0; i < number ; i++){
             randomArray.push(randomItem());    
-            
         }
         return randomArray;
     }
